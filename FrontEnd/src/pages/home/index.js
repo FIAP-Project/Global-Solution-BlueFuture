244ce06f7f6d70fdc61535bd8e5875b4e84495e9
Felipe <felipecerboncinix@gmail.com>
2024-06-06 10:21:01 -0300

    Reorganize JS

--- FrontEnd/src/pages/home/index.js
+++ FrontEnd/src/pages/home/index.js
@@ -1,59 +1,53 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const carousels = document.querySelectorAll('.carousel');
-
-    carousels.forEach((carousel) => {
-        let currentSlide = 0;
-        const slides = carousel.querySelectorAll('.carousel-item');
-        const nextButton = carousel.querySelector('.carousel-control.next');
-        const prevButton = carousel.querySelector('.carousel-control.prev');
-
-        function showSlide(index) {
-            if (index >= slides.length)
-                currentSlide = 0;
-
-            else if (index < 0)
-                currentSlide = slides.length - 1;
-            else
-                currentSlide = index;
-
-            slides.forEach((slide) => {
-                slide.style.transform = `translateX(-${currentSlide * 100}%)`;
-            });
-        }
-
-        nextButton.addEventListener('click', () => {
-            showSlide(currentSlide + 1);
-        });
-
-        prevButton.addEventListener('click', () => {
-            showSlide(currentSlide - 1);
-        });
-
-        // Initialize the carousel
-        showSlide(currentSlide);
-    });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    const questions = document.querySelectorAll('.question');
-
-    questions.forEach(question => {
-        question.addEventListener('click', () => {
-            const answer = question.nextElementSibling;
-            const toggleSign = question.querySelector('.toggle-sign');
-
-            if (answer.style.display === 'block') {
-                answer.style.display = 'none';
-                toggleSign.textContent = '+';
-            } else {
-                answer.style.display = 'block';
-                toggleSign.textContent = '−';
-            }
-        });
-    });
-});
-
+const carousels = document.querySelectorAll('.carousel');
+const questions = document.querySelectorAll('.question');
 const saibaMaisBtn = document.querySelector('#saiba-mais-btn')
+
+carousels.forEach((carousel) => {
+    let currentSlide = 0;
+    const slides = carousel.querySelectorAll('.carousel-item');
+    const nextButton = carousel.querySelector('.carousel-control.next');
+    const prevButton = carousel.querySelector('.carousel-control.prev');
+
+    function showSlide(index) {
+        if (index >= slides.length)
+            currentSlide = 0;
+
+        else if (index < 0)
+            currentSlide = slides.length - 1;
+        else
+            currentSlide = index;
+
+        slides.forEach((slide) => {
+            slide.style.transform = `translateX(-${currentSlide * 100}%)`;
+        });
+    }
+
+    nextButton.addEventListener('click', () => {
+        showSlide(currentSlide + 1);
+    });
+
+    prevButton.addEventListener('click', () => {
+        showSlide(currentSlide - 1);
+    });
+
+    // Initialize the carousel
+    showSlide(currentSlide);
+});
+
+questions.forEach(question => {
+    question.addEventListener('click', () => {
+        const answer = question.nextElementSibling;
+        const toggleSign = question.querySelector('.toggle-sign');
+
+        if (answer.style.display === 'block') {
+            answer.style.display = 'none';
+            toggleSign.textContent = '+';
+        } else {
+            answer.style.display = 'block';
+            toggleSign.textContent = '−';
+        }
+    });
+});
 
 saibaMaisBtn.addEventListener('click', () => {
     alert('Página de saiba mais não implementada ainda')
