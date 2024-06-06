30df4b98f88942d3259bc49ae9f395a1e3fdff5f
Felipe <felipecerboncinix@gmail.com>
2024-06-06 11:43:20 -0300

    Add a popup form and add validation to all entries

--- FrontEnd/src/pages/home/index.js
+++ FrontEnd/src/pages/home/index.js
@@ -1,6 +1,14 @@
 const carousels = document.querySelectorAll('.carousel');
-const questions = document.querySelectorAll('.question');
+
 const saibaMaisBtn = document.querySelector('#saiba-mais-btn')
+
+const questions = document.querySelectorAll('.question');
+
+const participeBtn = document.querySelector('#participe-btn')
+const popup = document.getElementById("popup");
+const closePopupButton = document.getElementById("closePopup");
+
+const form = document.querySelector('#createAccountForm')
 
 carousels.forEach((carousel) => {
     let currentSlide = 0;
@@ -52,3 +60,55 @@ questions.forEach(question => {
 saibaMaisBtn.addEventListener('click', () => {
     alert('Página de saiba mais não implementada ainda')
 })
+
+participeBtn.addEventListener("click", () => {
+    popup.style.display = "block";
+    document.body.classList.add("no-scroll");
+});
+
+closePopupButton.addEventListener("click", () => {
+    popup.style.display = "none";
+    document.body.classList.remove("no-scroll");
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let valid = true;
+
+    const name = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const tel = document.getElementById("telefone").value;
+
+    if (!validName(name) || !validEmail(email) || !validTel(tel))
+        valid = false
+
+    if (valid)
+        form.submit();
+})
+
+function validName(name) {
+    if (name.length < 3) {
+        alert("Name must be at least 3 characters long");
+        return false;
+    }
+    return true
+}
+
+function validEmail(email) {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+        alert("Invalid email address")
+        return false;
+    }
+    return true
+}
+
+function validTel(tel) {
+    const telPattern = /^\d{2} \d{5}-\d{4}$/;
+    if (!telPattern.test(tel)) {
+        alert("Invalid phone number format. Use 99 99999-9999");
+        return false;
+    }
+    return true
+}
